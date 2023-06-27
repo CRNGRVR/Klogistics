5c58d23bae3f8bd67cbefbbd955932ec23cb61b8
Ivan <i.a.konuhov@gmail.com>
2023-06-28 01:23:24 +0700

    Initial Commit

---
+++ Klogistics/KlogisticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlogisticsApp.swift
+//  Klogistics
+//
+//  Created by Иван on 28.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct KlogisticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
